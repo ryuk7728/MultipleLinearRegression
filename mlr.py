c140c0feebf2d3782d376919c0c6fe86f85807bc
ryuk7728 <ryuk7728@gmail.com>
2024-06-18 16:18:26 +0530

    Vectorized the implementations of the functions

--- mlr.py
+++ mlr.py
@@ -2,45 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def compute_cost(x,y,m,w,b):
-    cost=0
-    for i in range(m):
-        cost += ((np.dot(x[i],w)+b)-y[i])**2
-    cost = (1/(2*m))*cost
+
+    y_pred=np.dot(x,w)+b
+    cost=np.sum((y_pred-y)**2)/(2*m)
     return cost
 
 def derW(x,y,m,n,w,b):
-
-    derw=np.zeros(n)
-    for j in range(n):
-        tderw=0
-        for i in range(m):
-            tderw+=((np.dot(x[i],w)+b)-y[i])*x[i][j]
-        derw[j]=tderw
-        
-    derw/=m
     
-
+    y_pred=np.dot(x,w)+b
+    derw=np.dot(x.T,y_pred-y)/m
     return derw
 
 def derB(x,y,m,n,w,b):
     
-        derb=0
-        for i in range(m):
-            derb+=((np.dot(x[i],w)+b)-y[i])
-        derb/=m
+        y_pred=np.dot(x,w)+b
+        derb=np.sum(y_pred-y)/m
         return derb
 
-def grad_desc(x,y,m,n,w,b,iter,alpha,lossx,loss):
+def grad_desc(x,y,m,n,w,b,iter,alpha):
     
+    lossx=[]
+    loss=[]
     for i in range(iter):
-         lossx=np.append(lossx,i)
-         loss=np.append(loss,compute_cost(x,y,m,w,b))
+         lossx.append(i)
+         loss.append(compute_cost(x,y,m,w,b))
          tmp_w=w-alpha*derW(x,y,m,n,w,b)
          tmp_b=b-alpha*derB(x,y,m,n,w,b)
          w=tmp_w
          b=tmp_b
     
-    plt.scatter(lossx,loss,color='red')
+    plt.plot(lossx,loss,color='red')
     plt.show()
     return w,b
 
@@ -53,10 +44,8 @@ n=np.shape(x_train)[1]
 w=np.zeros((n,))
 b=0
 alpha=0.0000001
-iter=100000
-lossx=np.array([])
-loss=np.array([])
-w,b=grad_desc(x_train,y_train,m,n,w,b,iter,alpha,lossx,loss)
+iter=100
+w,b=grad_desc(x_train,y_train,m,n,w,b,iter,alpha)
 
 
 print(f"Weights obtained:{w}, Bias Obtained: {b}")
