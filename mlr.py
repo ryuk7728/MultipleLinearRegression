8c1568a544576729257646e232740800cb66f397
ryuk7728 <ryuk7728@gmail.com>
2024-06-18 20:21:37 +0530

    Implemented feature scaling

--- mlr.py
+++ mlr.py
@@ -23,6 +23,7 @@ def grad_desc(x,y,m,n,w,b,iter,alpha):
     
     lossx=[]
     loss=[]
+    finloss=0
     for i in range(iter):
          lossx.append(i)
          loss.append(compute_cost(x,y,m,w,b))
@@ -30,22 +31,27 @@ def grad_desc(x,y,m,n,w,b,iter,alpha):
          tmp_b=b-alpha*derB(x,y,m,n,w,b)
          w=tmp_w
          b=tmp_b
+         if(i==iter-1):
+              finloss=compute_cost(x,y,m,w,b)
     
     plt.plot(lossx,loss,color='red')
     plt.show()
-    return w,b
+    return w,b,finloss
 
 
 
 x_train=np.array([[2104,5,1,45],[1416,3,2,40],[852,2,1,35]])
+xmax=np.max(x_train)
+x_train=x_train/xmax
 y_train=np.array([460,232,178])
 m=np.shape(x_train)[0]
 n=np.shape(x_train)[1]
 w=np.zeros((n,))
 b=0
-alpha=0.0000001
-iter=100
-w,b=grad_desc(x_train,y_train,m,n,w,b,iter,alpha)
+alpha=1
+iter=1000000
+w,b,finloss=grad_desc(x_train,y_train,m,n,w,b,iter,alpha)
 
 
-print(f"Weights obtained:{w}, Bias Obtained: {b}")
+
+print(f"Weights obtained:{w}, Bias Obtained: {b}, Final Loss:{finloss}")
